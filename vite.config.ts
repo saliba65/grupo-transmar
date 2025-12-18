652d36408c962bc72f68f88b4b011c53134acad5
Lucas Saliba <lucassaliba10@gmail.com>
2025-12-18 14:07:02 -0300

    Fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  base: "grupo-transmar", // Use o nome exato do seu repo no GitHub
+  base: "/grupo-transmar/", // Use o nome exato do seu repo no GitHub
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
